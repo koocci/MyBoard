a80dcd9413c212c0b4d776fed35c1e3f5027bf04
jinhyun.koo <lazyeffect1@gmail.com>
2023-01-29 23:52:53 +0900

    feat(setting): set checkstyle
    add config about checkstyle
    
    Signed-off-by: jinhyun.koo <lazyeffect1@gmail.com>

--- src/main/java/com/tistory/kooccidev/myboard/MyBoardApplication.java
+++ src/main/java/com/tistory/kooccidev/myboard/MyBoardApplication.java
@@ -3,11 +3,19 @@ package com.tistory.kooccidev.myboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type My board application.
+ */
 @SpringBootApplication
 public class MyBoardApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyBoardApplication.class, args);
-    }
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(MyBoardApplication.class, args);
+  }
 
 }
